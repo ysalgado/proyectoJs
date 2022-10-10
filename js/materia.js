aa437367992ac2cda01cf369ec4558e2b87ff872
YaninSalgado <salgado_yanina@yahoo.com.ar>
2022-10-10 20:23:54 -0300

    subida 2da parte

--- js/materia.js
+++ js/materia.js
@@ -1,18 +1,34 @@
 
 
+/*let quieroInscribirme = document.getElementById("button").onclick=prompt ("Estas seguro de inscribirte?")*/
+
+
+let materiaInternacional1 = {
+    nombre: "economia internacional",
+    cupo: 20,
+    modalidad: "presencial",
+}
+
+let materiaInternacional2 = {
+    nombre: "ejecucion y control en entornos inciertos",
+    cupo: 30,
+    modalidad: "hibrida",
+}
+
+
 let cupo = 0
     
 function materia () {
     let respuesta = prompt ("Ingresa la materia optativa Internacional a la que queres inscribirte");
-    if (respuesta == "economia"){
+    if (respuesta == "economia internacional"){
         alert("ok")
     } else if (respuesta !== "economia"){
-        alert ("Esa materia no existe")
+        alert ("Lo siento ingresaste una materia incorrecta")
     }
-        if (respuesta == "economia" && cupo <= 20){
+        if (respuesta == "economia internacional" && cupo <= 20){
         cupo = cupo +1;
         alert("inscripto!")
-    } else if (respuesta !== "economia") {
+    } else if (respuesta !== "economia internacional") {
         alert ("Selecciona otra materia.")
     }
     else {
